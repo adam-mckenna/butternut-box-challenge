516bba5f76a887b70cc0ddd8e7dd56ba524dbbb1
Adam McKenna <adam-mckenna@outlook.com>
2022-06-29 11:24:32 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <h2>Choose your delivery day</h2>
+    <p>Delivery is always free</p>
+    <div className="card">
+      <p>Date...</p>
+      <span>Earliest delivery...</span>
+
+      <button>
+        Change
+      </button>
+    </div>
+  </div>
+)
+
+export default App
